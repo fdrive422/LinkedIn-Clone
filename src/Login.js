1b91d43ef9a48f28ec5788348f5abf59dcfe79d6
fdrive422 <fafrago@gmail.com>
2022-09-11 19:55:24 -0700

    setup redux and inital login js file

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Login.css';
+
+function Login() {
+	return (
+		<div className="login">
+			<img src='https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lRoVks' alt='' />
+		</div>
+	);
+}
+
+export default Login;
